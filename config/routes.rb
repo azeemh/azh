2b43ef0605a518296217d6fac070e95fd30c989b
azeemh <azeem.hussein@gmail.com>
2019-08-19 13:34:58 -0400

    demo app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :libraries
+  resources :books
+  resources :authors
+  devise_for :users
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
